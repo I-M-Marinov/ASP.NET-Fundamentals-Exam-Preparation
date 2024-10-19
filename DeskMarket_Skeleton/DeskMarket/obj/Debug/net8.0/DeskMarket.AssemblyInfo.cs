820fd7581f265ddc444e9382796b1d1f4a1604d6
I-M-Marinov <lcfrrr@gmail.com>
2024-10-19 19:37:41 +0300

    Cleanup bin & obj folders
    Cleanup bin & obj folders

--- DeskMarket_Skeleton/DeskMarket/obj/Debug/net8.0/DeskMarket.AssemblyInfo.cs
+++ DeskMarket_Skeleton/DeskMarket/obj/Debug/net8.0/DeskMarket.AssemblyInfo.cs
@@ -11,12 +11,11 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-DeskMarket-827aa371-cf1d-4b52-96c7-d1b66594c1fe")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeskMarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a25d615390164efead5ca61f3f5ce4c38b685025")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2e04e358a83816988454b8b006e9309d92b34b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeskMarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeskMarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
